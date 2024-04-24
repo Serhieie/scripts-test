b43cd3efa49fd82ff9e3cd71d2c48f201b84e80e
Serhieie <89160714+Serhieie@users.noreply.github.com>
2024-04-24 09:20:36 +0300

    fix imports

--- find-work/scripts/robotaUa/index.js
+++ find-work/scripts/robotaUa/index.js
@@ -1,17 +1,5 @@
-// const parseBack = require('./parse/parseJuniorBackEnd');
-// const parseFront = require('./parse/parseJuniorFront');
-// const parseFull = require('./parse/parseJuniorFullStack');
-// const spamFull = require('./spam/spamJuniorBackEnd');
-// const spamBack = require('./spam/spamJuniorFront');
-// const spamFront = require('./spam/spamJuniorFullStack');
 const { spamScriptRabotaUA } = require('./spam/spamScriptRabotaUA');
 
 module.exports = {
-  // parseBack,
-  // parseFront,
-  // parseFull,
-  // spamFull,
-  // spamBack,
-  // spamFront,
   spamScriptRabotaUA,
 };
