5feeb50788b3a7f4683ba1d0dec29749e9f904f8
Serhieie <89160714+Serhieie@users.noreply.github.com>
2024-04-26 09:04:23 +0300

    test: done

--- find-work/middlewares/index.js
+++
@@ -1,6 +0,0 @@
-const isValidId = require('./isValidId');
-const validateBody = require('./validateBody');
-const autenticate = require('./autenticate');
-// const upload = require("./multerMd");
-
-module.exports = { isValidId, validateBody, autenticate };
